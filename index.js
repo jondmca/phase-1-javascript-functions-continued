881a016ce73057df8a8bdfe36223951b17308a24
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-22 17:31:47 -0500

    commit tree b7e3d504cfa3e665db518735112cbb091c04d56b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = "go to the office"){
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flare = "*"){
+    return function(adj = special){
+        return `You are ${flare}${adj}${flare}!`
+    }
+}
